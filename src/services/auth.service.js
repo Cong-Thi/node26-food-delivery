eb31c4ae2e114102fd6ff400cad6590655d9bb09
Cong-Thi <trancongthi@gmail.com>
2022-12-29 20:29:12 +0700

    bt

---
+++ src/services/auth.service.js
@@ -0,0 +1,36 @@
+const bcrypt = require("bcrypt")
+const { AppError } = require("../helpers/error");
+const { User } = require("../models/index");
+const { generateToken } = require("../helpers/jwt");
+
+
+const login = async (credentials) => {
+    try {
+        const {email, password} = credentials;
+        const user = await User.findOne({
+            where: { email },
+            attributes: { include: ["password"] },
+          });
+
+        if(!user){
+            throw new AppError(400,"Email or password invalid");
+        }
+
+        
+        const isMatched = bcrypt.compareSync(password, user.password);
+        if(!isMatched){
+            throw new AppError(400,"email or password invalid")
+        }
+
+        return generateToken(user);
+
+        //delete user.dataValues.password;
+        //return user;
+    } catch (error) {
+        throw error;
+    }
+};
+
+module.exports = {
+    login,
+}
